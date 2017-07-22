42dbc6c64af3c21a93263d2539399c0cad15d240
charlie <charlie@openschoolgames.com>
2017-07-22 14:04:14 -0500

    weight and length

--- osg-unit-conversion/src/main/java/com/openschoolgames/unitconversion/UnitConversion.java
+++ osg-unit-conversion/src/main/java/com/openschoolgames/unitconversion/UnitConversion.java
@@ -1,24 +1,38 @@
 package com.openschoolgames.unitconversion;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.openschoolgames.evaluator.Evaluator;
 import com.openschoolgames.reader.Reader;
 
 public class UnitConversion {
-	
-	public double convert(final double value, final String from, final String to){
-		final Reader reader = new Reader();
-		final String name = "temperature_conversions.csv";
-		int[] primaryKeys = { 0 };
-		final InputStream in = UnitConversion.class.getResourceAsStream(name);
-		final Map<String, Map<String, String>> unitConversionMap = reader.readCSV(primaryKeys, in);
+
+	public double convert(final double value, final String from, final String to) {
+		final String unitConversionExpression = getUnitConversionExpression(from, to);
 		final Map<String, Double> valueMap = new HashMap<String, Double>();
 		valueMap.put(from, value);
 		final Evaluator ev = new Evaluator();
 		ev.setValueMap(valueMap);
-		return ev.evaluate(unitConversionMap.get(from + " to " + to).get("expression"));
+		return ev.evaluate(unitConversionExpression);
+	}
+
+	private String getUnitConversionExpression(final String from, final String to) {
+		final List<String> conversionFiles = Arrays.asList("temperature_conversions", "weight_conversions", "length_conversions");
+		final Reader reader = new Reader();
+		int[] primaryKeys = { 0 };
+		for (final String conversionFile : conversionFiles) {
+			final InputStream in = UnitConversion.class.getResourceAsStream(conversionFile + ".csv");
+			if (in != null) {
+				final Map<String, Map<String, String>> unitConversionMap = reader.readCSV(primaryKeys, in);
+				if (unitConversionMap.get(from + " to " + to) != null) {
+					return unitConversionMap.get(from + " to " + to).get("expression");
+				}
+			}
+		}
+		return null;
 	}
 }
--- osg-unit-conversion/src/test/java/com/openschoolgames/unitconversion/UnitConversionTest.java
+++ osg-unit-conversion/src/test/java/com/openschoolgames/unitconversion/UnitConversionTest.java
@@ -47,4 +47,18 @@ public class UnitConversionTest {
 		assertEquals("0 degrees F", 255.37222222, uc.convert(0.0, "f", "k"), 0.00001);
 		assertEquals("-459.67 degrees F", 0.0, uc.convert(-459.67, "f", "k"), 0.00001);
 	}
+	
+	@Test
+	public void ozToLbTest() {
+		UnitConversion uc = new UnitConversion();
+		assertEquals("16 oz", 1.0, uc.convert(16.0, "oz", "lb"), 0.00001);
+		assertEquals("0 oz", 0.0, uc.convert(0.0, "oz", "lb"), 0.00001);
+	}
+	
+	@Test
+	public void lbToOzTest() {
+		UnitConversion uc = new UnitConversion();
+		assertEquals("1 lb", 16.0, uc.convert(1.0, "lb", "oz"), 0.00001);
+		assertEquals("0 lb", 0.0, uc.convert(0.0, "lb", "oz"), 0.00001);
+	}
 }
